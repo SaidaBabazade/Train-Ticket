2b3d17352ad9b8db6854f91b467da1dcd00b4c96
Saida <babazade_seide@mail.ru>
2024-02-16 14:43:22 +0400

    first commit

---
+++ src/test/java/az/trainticket/TrainTicket/TrainTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package az.trainticket.TrainTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
